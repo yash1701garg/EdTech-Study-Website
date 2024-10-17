ad790d87a4d168e50fa542a0d07f93b660994d28
yash1701garg <yashgarg1701@gmail.com>
2024-10-17 22:25:42 +0530

    working on middleware and controllers

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -3,6 +3,9 @@ const Profile = require('../models/Profile');
 const OTP = require('../models/OTP');
 const otpGenerator = require('otp-generator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { mailSender } = require('../utils/mailSender');
+require('dotenv').config();
 //send otp
 exports.sendOTP = async (req,res) => {
     try {
@@ -132,14 +135,115 @@ exports.signUp = async (req,res) => {
 //login
 exports.login = async(req,res) => {
     try {
-        
+        //fetch data
+        const {email,password} = req.body;
+        //
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter full details'
+            });
+        }
+
+        //find user
+        const user = await User.findOne({email}).populate('additionalDetails');
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:'User is not regiser , Please SignUp',
+            })
+        };
+
+        if(await bcrypt.compare(password,user.password)){
+            //make the token
+            const token = jwt.sign(
+                { email: user.email, id: user._id, role: user.role },
+                process.env.JWT_SECRET_KEY,
+                {
+                    expiresIn:'24h',
+                }
+            );
+            //saved the entry in db
+            user.token = token;
+            user.password = undefined;
+            //send the cookie
+            const option = {
+               expires:  new Date(Date.now()) + 3*24*60*60*1000,
+               httpOnly:true,
+            }
+            res.cookie("token",token,option).status(200).json({
+                success:true,
+                message:'User login successfully!!',
+            });
+        }
+        else{
+            return res.status(400).json({
+                success: false,
+                message: `Password is incorrect`,
+            })
+        }
     } catch (error) {
-        
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:'Login failure , Please try again',
+        })
     }
 }
 
 
 //change password 
+exports.changePassword = async (req,res) => {
+    try {
+        //get user data 
+        const userDetails = await User.findById(req.user.id);
+        //get old password and new password
+        const {oldPassword,newPassword} = req.bodyl
+        //validata old password
+        const isPasswordMatch = await bcrypt.compare(oldPassword,userDetails.password);
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                success:false,
+                message:'Please enter the correct password',
+            })
+        };
+        //update old password by bcyrpt
+        const encryptedPassword = await bcrypt.hash(newPassword,10);
+        const updatedUserDetails = await User.findByIdAndUpdate(
+            req.user.id,
+            {password:encryptedPassword},
+            {new:true},
+        );
+        //send mail notification
+        try {
+            const emailResponse = await mailSender(
+                updatedUserDetails.password,"Password for your account is updated",
+                passwordUpdated(
+                    updatedUserDetails.email,
+                    `Password updated successfully for ${updatedUserDetails.firstName} ${updatedUserDetails.lastName}`
+                  )
+            );
+            console.log("Email sent successfully:", emailResponse.response)
+        } catch (error) {
+            console.error("Error occurred while sending email:", error)
+            return res.status(500).json({
+              success: false,
+              message: "Error occurred while sending email",
+              error: error.message,
+            })
+          }
+          return res.status(200).json({
+            success:true,
+            message:"Password has been updated",
+          })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:'Change Password failure , Please try again',
+        })
+    }
+}
 
 
 
